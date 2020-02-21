b736b08152cd2cfe5214c98c7632630e2a4a02ba
Henri <hloukonen@gmail.com>
2020-02-21 15:02:31 +0200

    Initial Commit

---
+++ BudjetointiApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BudjetointiApp
+//
+//  Created by Henri Loukonen on 21.2.2020.
+//  Copyright © 2020 Henri Loukonen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
